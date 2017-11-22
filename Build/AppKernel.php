2c2dbde43a6273c3c6e53e7332ca26d7f4e074bb
BadPixxel <contact@splashsync.com>
2017-11-22 15:07:52 +0100

    Work on Travis-Ci

--- Build/AppKernel.php
+++ Build/AppKernel.php
@@ -18,15 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             
             new Splash\Tasking\SplashTaskingBundle(),
-            
-            // These are the other bundles the SonataAdminBundle relies on
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\BlockBundle\SonataBlockBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-
-            // And finally, the storage and SonataAdminBundle
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-            new Sonata\AdminBundle\SonataAdminBundle(),               
+                        
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
