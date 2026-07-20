c713a7d50f08883c5f07f3c32f08cfd02b682908
BadPixxel <eshop.bpaquier@gmail.com>
2026-07-20 17:40:21 +0200

    WIP: Refactor Configuration.php to improve Symfony compatibility; update type hints, add assertions, and simplify children node configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,9 +15,11 @@
 
 namespace BadPixxel\Tasking\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Webmozart\Assert\Assert;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
@@ -25,9 +27,9 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 class Configuration implements ConfigurationInterface
 {
     /**
-     * @var NodeDefinition
+     * @var ArrayNodeDefinition
      */
-    private NodeDefinition $rootNode;
+    private ArrayNodeDefinition $rootNode;
 
     /**
      * {@inheritdoc}
@@ -35,7 +37,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('badpixxel_tasking');
-        $this->rootNode = $treeBuilder->getRootNode();
+        //====================================================================//
+        // Root Node Type Hints Differs with Symfony Versions
+        /** @var NodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
+        Assert::isInstanceOf($rootNode, ArrayNodeDefinition::class);
+        $this->rootNode = $rootNode;
 
         $this->configureCommonParameters();
         $this->configureServerParameters();
@@ -53,75 +60,64 @@ class Configuration implements ConfigurationInterface
      */
     private function configureCommonParameters(): void
     {
-        // @phpstan-ignore-next-line
-        $this->rootNode
-            ->children()
-            //====================================================================//
-            // COMMON Parameters
-            //====================================================================//
-            ->scalarNode('environment')
+        //====================================================================//
+        // COMMON Parameters
+        //====================================================================//
+        $children = $this->rootNode->children();
+        $children->scalarNode('environment')
             ->defaultValue('prod')
             ->cannotBeEmpty()
             ->info('Specify the environnement to use for running background tasks.')
-            ->end()
-            ->scalarNode('entity_manager')
+        ;
+        $children->scalarNode('entity_manager')
             ->defaultValue('default')
             ->cannotBeEmpty()
             ->info('Specify the Doctrine Entity Manager to use for storing tasks.')
-            ->end()
-            ->integerNode('refresh_delay')
+        ;
+        $children->integerNode('refresh_delay')
             ->defaultValue(3)
             ->info('Time in seconds between two status refresh of a worker.')
-            ->end()
-            ->integerNode('watchdog_delay')
+        ;
+        $children->integerNode('watchdog_delay')
             ->defaultValue(30)
             ->info(
-                'Max. Time in seconds between two status refresh of a worker. 
+                'Max. Time in seconds between two status refresh of a worker.
                 If exceeded, worker is considered as faulty and restarted.'
             )
-            ->end()
-            ->booleanNode('multiserver')
+        ;
+        $children->booleanNode('multiserver')
             ->defaultValue(false)
             ->info('Enable Multi-Server mode. Allow activation of Workers on a Pool of Servers')
-            ->end()
-            ->scalarNode('multiserver_path')
+        ;
+        $children->scalarNode('multiserver_path')
             ->defaultValue("/tasking/start")
             ->info('Additional Path for Multi-Server mode.')
-            ->end()
-            ->end()
         ;
     }
 
     /**
      * Add Server Parameters To Configuration
      *
-     * @return $this
+     * @return void
      */
-    private function configureServerParameters(): self
+    private function configureServerParameters(): void
     {
-        // @phpstan-ignore-next-line
-        $this->rootNode
-            ->children()
-            //====================================================================//
-            // SERVER Parameters
-            //====================================================================//
+        //====================================================================//
+        // SERVER Parameters
+        //====================================================================//
+        $children = $this->rootNode->children()
             ->arrayNode('server')
             ->addDefaultsIfNotSet()
             ->children()
-            ->booleanNode('force_crontab')
+        ;
+        $children->booleanNode('force_crontab')
             ->defaultValue(false)
             ->info('Enable to force setup of users crontab. When disabled you need to manage crontab manually')
-            ->end()
-            ->scalarNode('php_version')
+        ;
+        $children->scalarNode('php_version')
             ->defaultValue("php")
             ->info('PHP Cli line to use for starting bash commands.')
-            ->end()
-            ->end()
-            ->end()
-            ->end()
         ;
-
-        return $this;
     }
 
     /**
@@ -131,34 +127,29 @@ class Configuration implements ConfigurationInterface
      */
     private function configureSupervisorParameters(): void
     {
-        // @phpstan-ignore-next-line
-        $this->rootNode
-            ->children()
-            //====================================================================//
-            // SUPERVISOR Parameters
-            //====================================================================//
+        //====================================================================//
+        // SUPERVISOR Parameters
+        //====================================================================//
+        $children = $this->rootNode->children()
             ->arrayNode('supervisor')
             ->addDefaultsIfNotSet()
             ->children()
-            ->integerNode('max_age')
+        ;
+        $children->integerNode('max_age')
             ->defaultValue(3600)
             ->info('Max. Age for a Supervisor Process in seconds. Supervisor Worker will stop after this delay.')
-            ->end()
-            ->scalarNode('refresh_delay')
+        ;
+        $children->scalarNode('refresh_delay')
             ->defaultValue(500)
             ->info('Delay between two Supervisor Worker Status checks in MilliSeconds.')
-            ->end()
-            ->integerNode('max_workers')
+        ;
+        $children->integerNode('max_workers')
             ->info('Number of active worker on same machine.')
             ->defaultValue(3)
-            ->end()
-            ->integerNode('max_memory')
+        ;
+        $children->integerNode('max_memory')
             ->info('Maximum Memory usage for Supervisor. Exit when reached.')
             ->defaultValue(100)
-            ->end()
-            ->end()
-            ->end()
-            ->end()
         ;
     }
 
@@ -169,30 +160,25 @@ class Configuration implements ConfigurationInterface
      */
     private function configureWorkersParameters(): void
     {
-        // @phpstan-ignore-next-line
-        $this->rootNode
-            ->children()
-            //====================================================================//
-            // WORKERS Parameters
-            //====================================================================//
+        //====================================================================//
+        // WORKERS Parameters
+        //====================================================================//
+        $children = $this->rootNode->children()
             ->arrayNode('workers')
             ->addDefaultsIfNotSet()
             ->children()
-            ->integerNode('max_tasks')
+        ;
+        $children->integerNode('max_tasks')
             ->defaultValue(100)
             ->info('Maximum task executed by a Worker. Restart when reached.')
-            ->end()
-            ->integerNode('max_age')
+        ;
+        $children->integerNode('max_age')
             ->defaultValue(120)
             ->info('Maximum lifetime for a Worker. Restart when reached.')
-            ->end()
-            ->integerNode('max_memory')
+        ;
+        $children->integerNode('max_memory')
             ->info('Maximum Memory usage for a Worker. Restart when reached.')
             ->defaultValue(200)
-            ->end()
-            ->end()
-            ->end()
-            ->end()
         ;
     }
 
@@ -203,30 +189,25 @@ class Configuration implements ConfigurationInterface
      */
     private function configureTasksParameters(): void
     {
-        // @phpstan-ignore-next-line
-        $this->rootNode
-            ->children()
-            //====================================================================//
-            // TASKS Parameters
-            //====================================================================//
+        //====================================================================//
+        // TASKS Parameters
+        //====================================================================//
+        $children = $this->rootNode->children()
             ->arrayNode('tasks')
             ->addDefaultsIfNotSet()
             ->children()
-            ->integerNode('max_age')
+        ;
+        $children->integerNode('max_age')
             ->defaultValue(180)
             ->info('Delay before a Completed Task is Deleted from Database.')
-            ->end()
-            ->integerNode('try_count')
+        ;
+        $children->integerNode('try_count')
             ->info('Number of failure of a Task before considering it finished.')
             ->defaultValue(5)
-            ->end()
-            ->integerNode('try_delay')
+        ;
+        $children->integerNode('try_delay')
             ->defaultValue(120)
             ->info('Delay before restarting a task that fails. In Seconds')
-            ->end()
-            ->end()
-            ->end()
-            ->end()
         ;
     }
 }
